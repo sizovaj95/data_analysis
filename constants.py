35bf9f85c924852d75329967c6711449dae30147
Julia <54853811+sizovaj95@users.noreply.github.com>
2022-03-31 17:35:18 +0100

    Moved descriptions analysis to separate file. Started pairing amalysis

---
+++ constants.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+wine_table_name = "winemag-data_first150k.csv"
+
+data_dir = Path('__file__').parent.resolve()/ 'data'
+
+country = "country"
+description = "description"
+points = "points"
+price = "price"
+variety = "variety"
+winery = "winery"
+province = "province"
+points_group = "points_group"
+log10_price = "log10_price"
