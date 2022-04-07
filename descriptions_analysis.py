d5cd095f939fd36bf628787fbc428a35d79c150c
Julia <54853811+sizovaj95@users.noreply.github.com>
2022-04-07 08:55:01 +0100

    Continue work on description analysis

--- descriptions_analysis.py
+++ descriptions_analysis.py
@@ -30,6 +30,8 @@ pair_with_re = [r"(enjoy(ed)?|pair(ed?)|\btry\b|served?|drink|combination|just t
                 r"wants some .{,30}cooking to go with it",
                 r"with wide variety of (fare|food|dish)"]
 
+TAGGED_WORDS = List[Tuple[str, str]]
+
 
 def load_food_from_nltk():
     # TODO maybe don't need it
@@ -94,12 +96,115 @@ def combine_results_of_two_taggers(tagged_words_main: List[tuple], tagged_words_
     return tagged_words_main
 
 
+def split_and_strip_food_phrase(tagged_words: TAGGED_WORDS, custom_tags: List[str]):
+    """
+    rich meats or mushrooms
+    meat or cheese
+    hearty pasta or rich meats
+    grilled beef or lamb
+    pork ribs or flank steak
+    casual chicken or fish
+    chocolate or aged cheese
+    roast chicken in herbes de Provence or ratatouille
+
+    roasted meats and savory pies
+    shellfish and salads
+    halibut and shellfish
+    sunny days and shellfish
+
+    simple{NN} , white(JJ) fish(SEAFOOD)
+    lamb(MEAT) , fresh(JJ) fish(SEAFOOD)
+
+    make look nicer
+    , like sautéed fish with a classic French sauce
+    a platter of grilled meats
+    that shellfish
+    a steak
+    simple , white fish
+
+    leave as is
+    elaborate roasts and stews
+    """
+    pairing_as_str = ' '.join([tup[0] for tup in tagged_words])
+    pos_as_str = ' '.join([tup[1] for tup in tagged_words])
+    pairings = [pairing_as_str]
+    descriptive_pos = ["JJ", "VBN", "NN"]
+    for word, tag in tagged_words:
+        if tag == "CC":
+            before_tag, after_tag = _split_by_pos_tag(tagged_words, 'CC')
+            pairings = [' '.join(_get_words_list(before_tag)), ' '.join(_get_words_list(after_tag))]
+            # if word.lower() == 'or':
+            #     pairings = pairing_as_str.split('or')
+            # elif word.lower() == "and":
+            #     if len(tagged_words) == 3 and (tagged_words[0][1] in custom_tags and tagged_words[2][1] in custom_tags):
+            #         pairings = pairing_as_str.split('and')
+        elif tag == ",":
+            before_tag, after_tag = _split_by_pos_tag(tagged_words, ',')
+            if _check_if_pos_tag_in_list(before_tag, custom_tags) and _check_if_pos_tag_in_list(after_tag, custom_tags):
+                pairings = [' '.join(_get_words_list(before_tag)), ' '.join(_get_words_list(after_tag))]
+            else:
+                pairings = [' '.join(_get_words_list(tagged_words))]
+
+    pairings = [p.strip() for p in pairings]
+    return pairings
+
+
+def _get_words_list(tagged_words: TAGGED_WORDS) -> List[str]:
+    return [tup[0] for tup in tagged_words]
+
+
+def _get_pos_list(tagged_words: TAGGED_WORDS) -> List[str]:
+    return [tup[1] for tup in tagged_words]
+
+
+def _check_if_pos_tag_in_list(tagged_words: TAGGED_WORDS, check_tag_list: List[str]) -> bool:
+    pos_list = _get_pos_list(tagged_words)
+    for check_tag in check_tag_list:
+        if check_tag in pos_list:
+            return True
+    return False
+
+
+def _split_by_pos_tag(tagged_words: TAGGED_WORDS, splitting_tag: str) -> Tuple[TAGGED_WORDS, TAGGED_WORDS]:
+    before_tag = []
+    after_tag = []
+    tag_found = False
+    for word, tag in tagged_words:
+        if tag == splitting_tag:
+            tag_found = True
+            continue
+        if not tag_found:
+            before_tag.append((word, tag))
+        else:
+            after_tag.append((word, tag))
+    return before_tag, after_tag
+
+
+def extract_pairing_food(tagged_words: TAGGED_WORDS, custom_tags: List[str]) -> list:
+    descriptive_pos = ["JJ", "VBN", "NN"]
+    pairings = []
+    i = 1
+    for word, tag in tagged_words[::-1]:
+        if tag in custom_tags:
+            pairing_tup = [tup for tup in tagged_words[:-i+1]]
+            pairings = split_and_strip_food_phrase(pairing_tup, custom_tags)
+            pairing = [tup[0] for tup in tagged_words[:-i+1]]
+            food_category = [tup[1] for tup in tagged_words[:-i+1] if tup[1] in custom_tags]
+            pairing = ' '.join(pairing)
+            print(pairings)
+            break
+        i += 1
+    return pairings
+
+
 def main():
+    # TODO make it all a class (anything pos tagging related)
     data = pd.read_csv(co.data_dir / "wine_descriptions.csv")
     my_food_dict, my_food_list = load_my_food()
     regex_tagger = define_custom_regex_tagger(my_food_dict)
+    custom_tags = [t.upper() for t in my_food_dict.keys()]
     for i, row in data[:1500].iterrows():
-        text = row[data]
+        text = row[co.description]
         sentences = nltk.tokenize.sent_tokenize(text)
         for sent in sentences:
             if match := re.search('|'.join(pair_with_re), sent, re.I):
@@ -110,6 +215,12 @@ def main():
                 tagged_after_words_default = nltk.pos_tag(after_words)
                 tagged_after_words_regex = regex_tagger.tag(after_words)
                 tagged_words = combine_results_of_two_taggers(tagged_after_words_default, tagged_after_words_regex)
+                pairing = extract_pairing_food(tagged_words, custom_tags)
+                if pairing:
+                    print(sent)
+                    print('\n')
+                else:
+                    print(f"No pairing sentence: {sent}")
                 # print(sent)
                 # print(tagged_words)
                 # print('\n')
